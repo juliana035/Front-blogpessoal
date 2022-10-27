104479d3397ad0a1062464b3750b525799ca50e3
juliana035 <juliana.ferreira_cruz@yahoo.com.br>
2022-10-27 18:43:24 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+       <>
+       <h1 className= 'titulo'>Home</h1>
+       <img src="https://i.imgur.com/H88yIo2.png" alt=" tela inicial" className="img"/>
+      
+       
+       </>
+
+    );
+
+}
+export default Home;
