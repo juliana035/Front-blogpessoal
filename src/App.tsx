b08074e5515ec7502757458de27db73bb2465855
juliana035 <juliana.ferreira_cruz@yahoo.com.br>
2022-11-01 21:33:25 -0300

    login css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import{BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
-
 
 
 function App() {
   return (
-   <>
-  <Navbar />
-  <Home/>
-  <Footer/>
-  
-   </>
+   
+  <Router>
+<Navbar />
+<div style={{ minHeight: '100vh' }}>
+
+<Routes> // Antigo Switch
+<Route path="/" element={<Login />} />
+<Route path="/login" element={<Login />} />
+<Route path="/home" element={<Home />} />
+
+</Routes>
+</div>
+<Footer />
+</Router>
   );
 }
 
--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,34 +4,35 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Typography, Grid } from '@material-ui/core'
 import { Box } from '@mui/material';
+import './Footer.css';
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className = 'box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className = 'textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/JulianaCruz" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                                <FacebookIcon className = 'redes' />
                             </a>
                             <a href="https://www.instagram.com/ju_cruzzzz/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className = 'redes' />
                             </a>
                             <a href="https://www./linkedin.com/in/ju-cruz/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className = 'redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className = 'textos' >© 2020 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom className = 'textos' align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
